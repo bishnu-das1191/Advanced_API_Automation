ea4a21165414008256b77c493e4a8283b19da1e6
bishnu <bishnu.das1191@gmail.com>
2025-11-19 11:21:51 +0530

    Added AuthTokenProvider class to get the token Dynamically and role based access.
    introduced Enums to store constants

--- src/test/java/com/api/tests/UserDetailsAPITest.java
+++ src/test/java/com/api/tests/UserDetailsAPITest.java
@@ -3,6 +3,10 @@ package com.api.tests;
 import static io.restassured.RestAssured.*;
 
 import static com.api.utils.ConfigManager.*;
+
+import static com.api.utils.AuthTokenProvider.*;
+
+import static com.api.constant.Role.*;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import io.restassured.module.jsv.JsonSchemaValidator;
@@ -18,7 +22,7 @@ public class UserDetailsAPITest {
     public void userDetailsAPITest() throws IOException {
         // Placeholder for user details API test implementation
 
-        Header authHeader = new Header("Authorization","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZmlyc3RfbmFtZSI6ImZkIiwibGFzdF9uYW1lIjoiZmQiLCJsb2dpbl9pZCI6ImlhbWZkIiwibW9iaWxlX251bWJlciI6Ijg4OTk3NzY2NTUiLCJlbWFpbF9pZCI6Im1hcmtAZ21haWwuY29tIiwicGFzc3dvcmQiOiI1ZjRkY2MzYjVhYTc2NWQ2MWQ4MzI3ZGViODgyY2Y5OSIsInJlc2V0X3Bhc3N3b3JkX2RhdGUiOm51bGwsImxvY2tfc3RhdHVzIjowLCJpc19hY3RpdmUiOjEsIm1zdF9yb2xlX2lkIjo1LCJtc3Rfc2VydmljZV9sb2NhdGlvbl9pZCI6MSwiY3JlYXRlZF9hdCI6IjIwMjEtMTEtMDNUMDg6MDY6MjMuMDAwWiIsIm1vZGlmaWVkX2F0IjoiMjAyMS0xMS0wM1QwODowNjoyMy4wMDBaIiwicm9sZV9uYW1lIjoiRnJvbnREZXNrIiwic2VydmljZV9sb2NhdGlvbiI6IlNlcnZpY2UgQ2VudGVyIEEiLCJpYXQiOjE3NjMxOTEzOTV9.XQ4CwwJ0dU0LFIBtl1CHOY5llXS82eL4kCneAHEzzEQ");
+        Header authHeader = new Header("Authorization", getToken(FD));
 
         given()
                 .baseUri(getProperty("BASE_URI"))
---
+++ src/test/java/com/api/utils/AuthTokenProvider.java
@@ -0,0 +1,47 @@
+package com.api.utils;
+
+import static com.api.constant.Role.*;
+
+
+import com.api.constant.Role;
+import com.api.pojo.UserCredentials;
+import static io.restassured.RestAssured.*;
+import io.restassured.http.ContentType;
+import static org.hamcrest.Matchers.*;
+
+public class AuthTokenProvider {
+
+    private AuthTokenProvider() {
+        // private constructor to prevent instantiation
+    }
+
+    public static String getToken(Role role) {
+        // Make the request for the login api and we want to extract
+        // the token from the response
+
+        UserCredentials userCredentials = null;
+        if(role == FD){
+            userCredentials = new UserCredentials("iamfd", "password");
+        } else if(role == SUP){
+            userCredentials = new UserCredentials("iamsup", "password");
+        } else if(role == ENG){
+            userCredentials = new UserCredentials("iameng", "password");
+        } else if (role == QC) {
+            userCredentials = new UserCredentials("iamqc", "password");
+        }
+
+        return given()
+                .baseUri(ConfigManager.getProperty("BASE_URI"))
+                .contentType(ContentType.JSON)
+                .body(userCredentials)
+                .when()
+                .post("login")
+                .then()
+                .log().ifValidationFails()
+                .statusCode(200)
+                .body("message", equalTo("Success"))
+                .extract()
+                .body().jsonPath().getString("data.token");
+
+    }
+}
